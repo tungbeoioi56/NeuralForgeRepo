d18149c523ede8a0336b43d5ee4b70f80a579d97
tungbeoioi56 <savagecat7nam1311@outlook.com>
2024-05-10 11:14:51 +0700

    init

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-function uniquePathsWithObstacles(obstacleGrid) {
-  const m = obstacleGrid.length;
-  const n = obstacleGrid[0].length;
-  const dp = new Array(m).fill(0).map(() => new Array(n).fill(0));
-  if (obstacleGrid[0][0] === 1) return 0;
-  dp[0][0] = 1;
-  for (let i = 1; i < m; i++) {
-    if (obstacleGrid[i][0] === 0) dp[i][0] = dp[i - 1][0];
+function removeNthFromEnd(head, n) {
+  const dummy = new ListNode(0);
+  dummy.next = head;
+  let first = dummy;
+  let second = dummy;
+  for (let i = 1; i <= n + 1; i++) {
+    first = first.next;
   }
-  for (let j = 1; j < n; j++) {
-    if (obstacleGrid[0][j] === 0) dp[0][j] = dp[0][j - 1];
+  while (first !== null) {
+    first = first.next;
+    second = second.next;
   }
-  for (let i = 1; i < m; i++) {
-    for (let j = 1; j < n; j++) {
-      if (obstacleGrid[i][j] === 0) dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-    }
-  }
-  return dp[m - 1][n - 1];
+  second.next = second.next.next;
+  return dummy.next;
 }
