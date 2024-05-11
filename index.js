b70784839687f066faffdfca7cb1e073587b4289
tungbeoioi56 <savagecat7nam1311@outlook.com>
2024-05-11 17:18:20 +0700

    feat project

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-function removeNthFromEnd(head, n) {
-  const dummy = new ListNode(0);
-  dummy.next = head;
-  let first = dummy;
-  let second = dummy;
-  for (let i = 1; i <= n + 1; i++) {
-    first = first.next;
+function countSubstrings(s) {
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    extendPalindrome(i, i);
+    extendPalindrome(i, i + 1);
   }
-  while (first !== null) {
-    first = first.next;
-    second = second.next;
+  return count;
+  function extendPalindrome(left, right) {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      count++;
+      left--;
+      right++;
+    }
   }
-  second.next = second.next.next;
-  return dummy.next;
 }
