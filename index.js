22941d8c2c1dade7e973003c0f664fd47ee3bbc0
tungbeoioi56 <savagecat7nam1311@outlook.com>
2024-05-14 17:08:05 +0700

    test commit

--- index.js
+++ index.js
@@ -1,38 +1,9 @@
-function longestIncreasingPath(matrix) {
-  if (matrix.length === 0) return 0;
-  const rows = matrix.length;
-  const cols = matrix[0].length;
-  const dp = Array.from(Array(rows), () => Array(cols).fill(0));
-  let longestPath = 0;
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      longestPath = Math.max(longestPath, dfs(matrix, i, j, dp));
-    }
+function getIntersectionNode(headA, headB) {
+  let currA = headA;
+  let currB = headB;
+  while (currA !== currB) {
+    currA = currA ? currA.next : headB;
+    currB = currB ? currB.next : headA;
   }
-  return longestPath;
-  function dfs(matrix, i, j, dp) {
-    if (dp[i][j] !== 0) return dp[i][j];
-    const dirs = [
-      [0, 1],
-      [0, -1],
-      [1, 0],
-      [-1, 0],
-    ];
-    let maxPath = 1;
-    for (const dir of dirs) {
-      const newRow = i + dir[0];
-      const newCol = j + dir[1];
-      if (
-        newRow >= 0 &&
-        newRow < matrix.length &&
-        newCol >= 0 &&
-        newCol < matrix[0].length &&
-        matrix[newRow][newCol] > matrix[i][j]
-      ) {
-        maxPath = Math.max(maxPath, 1 + dfs(matrix, newRow, newCol, dp));
-      }
-    }
-    dp[i][j] = maxPath;
-    return maxPath;
-  }
+  return currA;
 }
